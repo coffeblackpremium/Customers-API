b9de84cc4d08c7dc1b0d11900ab69288a28927b4
João Damaceno <joaodamaceno188@gmail.com>
2022-10-10 17:33:19 -0400

    Primeiro Commit: First More Date in this course 'How to build a REST API With laravel: PHP Full Course' completed

---
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Customer;
+use App\Http\Resources\V1\CustomerResource;
+use App\Http\Resources\V1\CustomerCollection;
+use App\Filters\V1\CustomersFilter;
+
+class CustomerController extends Controller
+{
+
+    public function index(Request $request)
+    {
+        $filter = new CustomersFilter();
+        $queryItems = $filter->transform($request);
+
+        if(is_countable($queryItems) && count($queryItems) == 0)
+        {
+            return new CustomerCollection(Customer::paginate());
+        }else
+        {
+            return new CustomerCollection(Customer::where($queryItems)->paginate());
+        }
+
+    }
+
+    public function show(Customer $customer)
+    {
+        return new CustomerResource($customer);
+    }
+}   
---
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\InvoiceCollection;
+use App\Http\Resources\V1\InvoiceResource;
+use Illuminate\Http\Request;
+use App\Models\Invoice;
+use App\Filters\V1\InvoicesFilter;
+
+class InvoiceController extends Controller
+{
+    public function index(Request $request)
+    {
+        $filter = new InvoicesFilter();
+        $queryItems = $filter->transform($request);
+
+        if(is_countable($queryItems) && count($queryItems) == 0)
+        {
+            return new InvoiceCollection(Invoice::paginate());
+        }else
+        {
+            return new InvoiceCollection(Invoice::where($queryItems)->paginate());
+        }
+
+    }
+
+    public function show(Invoice $invoice)
+    {
+        return new InvoiceResource($invoice);
+    }
+}
